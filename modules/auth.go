236cc35882994958602ae2660cc13aba1779ca6b
黄金叠 <hjd_2008@foxmail.com>
2017-06-09 13:35:26 +0800

    临时权限控制

--- modules/auth.go
+++ modules/auth.go
@@ -11,10 +11,21 @@ type AuthInfo struct {
 }
 
 func (a *AuthInfo) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc)()  {
-	urlpsth:=r.URL.Path
+	urlpath:=r.URL.Path
 	session := sessions.GetSession(r)
-	if strings.HasPrefix(urlpsth,"/login") || strings.HasPrefix(urlpsth,"/static") || session.Get("user")!=nil{
-		next(rw, r)
+	if strings.HasPrefix(urlpath,"/login") || strings.HasPrefix(urlpath,"/static") || session.Get("user")!=nil{
+		role:=session.Get("role")
+		if role==nil{
+			if urlpath=="/sentinelschange"|| urlpath=="/sentinelsdel"||urlpath=="/redisschange"||
+				urlpath=="/redissdel"||urlpath=="/keysdel" ||urlpath=="/keysexpire"||urlpath=="/keyvaldel"||
+				urlpath=="/keyspersist"||urlpath=="/keysave" ||urlpath=="/keyrename"{
+				http.NotFound(rw,r)
+			}else {
+				next(rw, r)
+			}
+		}else {
+			next(rw, r)
+		}
 	}else {
 		http.Redirect(rw,r,"/login",http.StatusFound)
 	}
